b1efc22c7344bba7a0c2c27eb7d49d0a273e6f10
Zuzanna Sobkowiak <zuz@MacBooks-Air.localdomain>
2019-08-31 16:49:43 -0700

    8.1

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -9,17 +9,16 @@ import java.util.Objects;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile() throws IOException {
+    public void readFile() {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
-        Path path = Paths.get(file.getPath());
-        try {
-            Stream<String> fileLines = Files.lines(path);
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))){
             fileLines.forEach(System.out::println);
-        } catch (IOException e) {
-
-            System.out.println("Oh no!");
-
+        }catch(IOException e){
+            System.out.println("Oh no" + e);
+        }finally {
+            System.out.println("End");
         }
+
     }
 }
