53f11de5aec6a116dfb0be2f3072402359dd7971
Zuzanna Sobkowiak <zuz@MacBooks-Air.localdomain>
2019-09-07 21:47:42 -0700

    [8.4]

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,25 @@
+package com.kodilla.exception.test;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightFinder {
+    Map<String, Boolean> connections;
+
+    public FlightFinder(Map<String, Boolean> findTheFlight) {
+        this.connections = findTheFlight;
+    }
+    public void findTheFlight(Flight flight) throws RouteNotFoundException{
+        if(connections.containsKey(flight.getArrivalAirport())){
+            if(connections.get(flight.getArrivalAirport())){
+                System.out.println("Connection found: " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport());
+            }else{
+                System.out.println("Connection not found: "  + flight.getDepartureAirport() + " to " + flight.getArrivalAirport());
+            }
+        }else{
+            throw new RouteNotFoundException();
+        }
+    }
+
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
@@ -0,0 +1,27 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+
+public class RouteNotFoundException extends Exception {
+    public static void main(String[] args){
+        Flight flight1 = new Flight("Seattle", "London");
+        Flight flight2 = new Flight("Los Angeles", "San Diego");
+        Flight flight3 = new Flight("Everett", "Portland");
+        Flight flight4 = new Flight("Jackson Hole", "Boise");
+
+        HashMap<String, Boolean> airports = new HashMap<>();
+        airports.put(flight1.getArrivalAirport(), true);
+        airports.put(flight2.getArrivalAirport(), true);
+        airports.put(flight3.getArrivalAirport(), false);
+        airports.put(flight4.getArrivalAirport(), true);
+
+        FlightFinder flightFinder = new FlightFinder(airports);
+
+        try{
+           flightFinder.findTheFlight(flight3);
+        }catch(RouteNotFoundException e){
+            System.out.println(e);
+        }
+
+    }
+}
