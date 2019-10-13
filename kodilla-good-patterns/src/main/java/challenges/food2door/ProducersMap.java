19bdfa60b5952317e17b636cbde9696bbba2a076
Zuzanna Sobkowiak <zuz@MacBooks-Air.localdomain>
2019-10-13 13:09:10 -0700

    13.4

--- kodilla-good-patterns/src/main/java/challenges/food2door/ProducersMap.java
+++ kodilla-good-patterns/src/main/java/challenges/food2door/ProducersMap.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ProducersMap {
-    public Map<ProductProcess, Product> offer = new HashMap<ProductProcess, Product>();
+    private Map<ProductProcess, Product> offer = new HashMap<ProductProcess, Product>();
 
     public void addToMap(ProductProcess productProcess, Product product){
         offer.put(productProcess, product);
