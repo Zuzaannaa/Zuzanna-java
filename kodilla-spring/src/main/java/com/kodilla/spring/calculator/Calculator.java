a8a866988f2ab84c964eb37d423750dc77a631ac
Zuzanna Sobkowiak <zuz@MacBooks-Air.localdomain>
2019-10-14 22:02:18 -0700

    14.2

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,12 +1,14 @@
 package com.kodilla.spring.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Calculator {
     private Display display;
 
-    public Calculator(Display display) {
+    @Autowired
+    public void setDisplay(Display display){
         this.display = display;
     }
 
