5c9b30959a68aa00447b195997d2d343dea4b43a
Zuzanna Sobkowiak <zuz@MacBooks-Air.localdomain>
2019-11-09 09:32:18 -0800

    17.3

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Item.java
@@ -0,0 +1,77 @@
+package com.kodilla.hibernate.invoice;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "ITEMS")
+public class Item {
+    private int id;
+    private BigDecimal price;
+    private int quantity;
+    private BigDecimal value;
+    private Product product;
+
+    public Item(){
+
+    }
+
+    public Item(BigDecimal price, int quantity, BigDecimal value, Product product) {
+        this.price = price;
+        this.quantity = quantity;
+        this.value = value;
+        this.product = product;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "PRICE")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    private void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Column(name = "QUANTITY")
+    public int getQuantity() {
+        return quantity;
+    }
+
+    private void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Column(name = "VALUE")
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    private void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "PRODUCT_ID")
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,66 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@Transactional
+public class InvoiceDaoTestSuite {
+    @Autowired
+    InvoiceDao invoiceDao;
+    @Autowired
+    ItemDao itemDao;
+    @Autowired
+    ProductDao productDao;
+
+    @Test
+    public void testInvoiceDaoSave(){
+        //Given
+        Invoice invoice = new Invoice("21/43/77");
+
+        Product cucumber = new Product("Cucumber");
+        Product donut = new Product("Donut");
+        Product cheese = new Product("Cheese");
+
+        Item item1 = new Item(new BigDecimal(99), 12, new BigDecimal(45), cucumber);
+        Item item2 = new Item(new BigDecimal(21), 98, new BigDecimal(22), donut);
+        Item item3 = new Item(new BigDecimal(45), 89, new BigDecimal(88), cheese);
+
+        invoice.getItems().add(item1);
+        invoice.getItems().add(item2);
+        invoice.getItems().add(item3);
+
+        //When
+        invoiceDao.save(invoice);
+        int invoiceId = invoice.getId();
+        Optional<Invoice> invoicePersist = invoiceDao.findById(invoiceId);
+        String nameOfProduct = cucumber.getName();
+        int items = invoicePersist.get().getItems().size();
+
+        //Then
+        Assert.assertEquals("Cucumber", nameOfProduct);
+        Assert.assertEquals(3, items);
+
+        //CleanUp
+        invoiceDao.delete(invoice);
+
+
+
+
+
+
+
+    }
+}
